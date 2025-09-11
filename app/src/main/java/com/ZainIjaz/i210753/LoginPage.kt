079bface112254d463dfd608f88dcac1a0520d2c
zainejaz67 <zain.ejaz67@gmail.com>
2025-09-12 02:14:32 +0500

    Sign up Page and Continue from previous account page completed

--- app/src/main/java/com/ZainIjaz/i210753/LoginPage.kt
+++ app/src/main/java/com/ZainIjaz/i210753/LoginPage.kt
@@ -2,18 +2,17 @@ package com.ZainIjaz.i210753
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_page)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -21,10 +20,10 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginPage::class.java)
+        val signUpText = findViewById<TextView>(R.id.newaccount)
+        signUpText.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
-            finish()
-        }, 2000)
+        }
     }
 }
--- app/src/main/java/com/ZainIjaz/i210753/MainActivity2.kt
+++ app/src/main/java/com/ZainIjaz/i210753/MainActivity2.kt
@@ -2,18 +2,17 @@ package com.ZainIjaz.i210753
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -21,10 +20,11 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        Handler(Looper.getMainLooper()).postDelayed({
+        val backIcon = findViewById<ImageView>(R.id.back_icon)
+        backIcon.setOnClickListener {
             val intent = Intent(this, LoginPage::class.java)
             startActivity(intent)
-            finish()
-        }, 2000)
+            finish() // optional → closes current activity
+        }
     }
 }
