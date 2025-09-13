67f3fe9e81a6340874a923cef1537c8662dc8e72
zainejaz67 <zain.ejaz67@gmail.com>
2025-09-13 18:04:04 +0500

    chat page completed

---
+++ app/src/main/java/com/ZainIjaz/i210753/callpage.kt
@@ -0,0 +1,20 @@
+package com.ZainIjaz.i210753
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class callpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_callpage)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/ZainIjaz/i210753/chat.kt
+++ app/src/main/java/com/ZainIjaz/i210753/chat.kt
@@ -3,17 +3,16 @@ package com.ZainIjaz.i210753
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class chatsection : AppCompatActivity() {
+class chat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_chatsection)
+        setContentView(R.layout.activity_chat)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -23,16 +22,9 @@ class chatsection : AppCompatActivity() {
         val back = findViewById<ImageView>(R.id.backbutton)
         back.setOnClickListener {
             val intent =
-                Intent(this, mainpage::class.java) // replace HomePage with your target Activity
+                Intent(this, chatsection::class.java) // replace HomePage with your target Activity
             startActivity(intent)
             finish() // optional: close MainActivity3 after login
-        }
-
-        val chatLayout = findViewById<LinearLayout>(R.id.imagebar)
-        chatLayout.setOnClickListener {
-            val intent = Intent(this, chat::class.java) // class reference
-            startActivity(intent)
-            finish()
         }
 
     }
