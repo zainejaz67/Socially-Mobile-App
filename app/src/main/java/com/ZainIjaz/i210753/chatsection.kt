f160eee09e2d3752eb77c94aee9104543fa6890f
zainejaz67 <zain.ejaz67@gmail.com>
2025-09-13 17:15:04 +0500

    chat sction completed with intents

--- app/src/main/java/com/ZainIjaz/i210753/chatsection.kt
+++ app/src/main/java/com/ZainIjaz/i210753/chatsection.kt
@@ -3,34 +3,26 @@ package com.ZainIjaz.i210753
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class mainpage : AppCompatActivity() {
+class chatsection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_mainpage)
+        setContentView(R.layout.activity_chatsection)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val search = findViewById<ImageView>(R.id.searchbutton)
-        search.setOnClickListener {
-            val intent =
-                Intent(this, SearchPage::class.java) // replace HomePage with your target Activity
-            startActivity(intent)
-            finish() // optional: close MainActivity3 after login
-        }
 
-        val send = findViewById<ImageView>(R.id.sendmessage)
-        send.setOnClickListener {
+        val back = findViewById<ImageView>(R.id.backbutton)
+        back.setOnClickListener {
             val intent =
-                Intent(this, chatsection::class.java) // replace HomePage with your target Activity
+                Intent(this, mainpage::class.java) // replace HomePage with your target Activity
             startActivity(intent)
             finish() // optional: close MainActivity3 after login
         }
